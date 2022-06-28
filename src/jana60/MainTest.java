39036bb2b34f0b7192c65c0adb48e70b5830fca3
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-28 15:01:30 +0200

    Aggiunta gestione eccezioni per valori negativi

--- src/jana60/MainTest.java
+++ src/jana60/MainTest.java
@@ -4,15 +4,29 @@ public class MainTest {
 
 	public static void main(String[] args) {
 
-		//Test Rettangolo
-		System.out.println("Sono un rettangolo!");
-		Rettangolo p1 = new Rettangolo(8.5, 5);
-		System.out.println(p1.toString());
+		try 
+		{
+			//Test Rettangolo
+			System.out.println("\nSono un rettangolo!");
+			Rettangolo p1 = new Rettangolo(8.5, 5);
+			System.out.println(p1.toString());
+			
+			//Test Triangolo
+			System.out.println("\nSono un triangolo!");
+			Triangolo p2 = new Triangolo(7, 11, 5, 4);
+			System.out.println(p2.toString());
+			
+			//Test Triangolo con Eccezione
+			System.out.println("\nSono un triangolo che rompe il codice!");
+			Triangolo p3 = new Triangolo(7, -11, 5, 4);
+			System.out.println(p3.toString());	
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("\nQualcosa è andato storto:");
+			System.out.println(	e.getMessage());
+		}
 		
-		//Test Triangolo
-		System.out.println("Sono un triangolo!");
-		Triangolo p2 = new Triangolo(7, 11, 5, 4);
-		System.out.println(p2.toString());
 	}
 
 }
--- src/jana60/Triangolo.java
+++ src/jana60/Triangolo.java
@@ -12,35 +12,66 @@ public class Triangolo implements Poligono {
 	private double base, altezza, lato1, lato2;
 	
 	//COSTRUTTORI
-	public Triangolo(double base, double altezza, double lato1, double lato2) {
+	public Triangolo(double base, double altezza, double lato1, double lato2) throws Exception 
+	{
+		boolean datiValidi = true; //Tengo traccia degli errori e dei rispettivi messaggi
+		String mexErrore = "";
 		
-		this.altezza = altezza;
-		this.base = base;
-		this.lato1 = lato1;
-		this.lato2 = lato2;
+		try //Valido dati prima di salvarli
+		{
+			validaDato(base);
+			validaDato(altezza);
+			validaDato(lato1);
+			validaDato(lato2);
+		} 	
+		catch (IllegalArgumentException iae) 
+		{
+			datiValidi = false;
+			mexErrore += " " + iae.getMessage();
+		}
 		
+		if (datiValidi) 
+		{	
+			this.altezza = altezza;
+			this.base = base;
+			this.lato1 = lato1;
+			this.lato2 = lato2;
+		} 
+		else 
+		{
+			throw new Exception(mexErrore);
+		}
+		
+		
+	}
+	
+	//EXCEPTIONS
+	private void validaDato(double dato) throws IllegalArgumentException
+	{
+		if (dato <= 0)
+			throw new IllegalArgumentException("ATTENZIONE: Nessuno dei dati può essere minore/uguale a 0!");	
 	}
 	
 	//METODI
 	@Override
-	public double calcolaPerimetro() {
+	public double calcolaPerimetro() 
+	{
 		return this.base + this.lato1 + this.lato2;
 	}
 	
-	
 	@Override
-	public double calcolaArea() {
+	public double calcolaArea() 
+	{
 		return this.base * this.altezza / 2.00;
 	}
 	
 	@Override
-	public String toString(){
-		
+	public String toString()
+	{
 		return 	"\n"	+ "Base: "		+ this.base						+
 				"\n"	+ "Altezza:"	+ this.altezza					+
-				"\n"	+ "Perimetro: "	+ cm.format(calcolaPerimetro())	+ 
-				"\n"	+ "Area: "		+ cm2.format(calcolaArea())		;
-		
+				"\n"	+ "Perimetro: "	+ cm.format(calcolaPerimetro())	+
+				"\n"	+ "Area: "		+ cm2.format(calcolaArea())		;	
 	}
 
 }
