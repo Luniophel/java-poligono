f53e70a12205fff0989c521e49ecf4ed242a422e
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-28 15:04:45 +0200

    Aggiunta gestione eccezioni per valori negativi Rettangolo()

--- src/jana60/Rettangolo.java
+++ src/jana60/Rettangolo.java
@@ -12,33 +12,60 @@ public class Rettangolo implements Poligono {
 	private double base, altezza;
 	
 	//COSTRUTTORI
-	public Rettangolo(double base, double altezza) {
+	public Rettangolo(double base, double altezza) throws Exception
+	{
+		boolean datiValidi = true; //Tengo traccia degli errori e dei rispettivi messaggi
+		String mexErrore = "";
 		
-		this.altezza = altezza;
-		this.base = base;
+		try //Valido dati prima di salvarli
+		{
+			validaDato(base);
+			validaDato(altezza);
+		} 	
+		catch (IllegalArgumentException iae) 
+		{
+			datiValidi = false;
+			mexErrore += " " + iae.getMessage();
+		}
 		
+		if (datiValidi) 
+		{	
+			this.altezza = altezza;
+			this.base = base;
+		} 
+		else 
+		{
+			throw new Exception(mexErrore);
+		}
 	}
 	
 	//METODI
+	private void validaDato(double dato) throws IllegalArgumentException
+	{
+		if (dato <= 0)
+			throw new IllegalArgumentException("ATTENZIONE: Nessuno dei dati può essere minore/uguale a 0!");	
+	}
+	
 	@Override
-	public double calcolaPerimetro() {
+	public double calcolaPerimetro() 
+	{
 		return this.altezza * 2.00 + this.base * 2.00;
 	}
 	
 	
 	@Override
-	public double calcolaArea() {
+	public double calcolaArea() 
+	{
 		return this.altezza * this.base;
 	}
 	
 	@Override
-	public String toString(){
-		
+	public String toString()
+	{		
 		return 	"\n"	+ "Base: "		+ this.base						+
 				"\n"	+ "Altezza:"	+ this.altezza					+
-				"\n"	+ "Perimetro: "	+ cm.format(calcolaPerimetro())	+ 
-				"\n"	+ "Area: "		+ cm2.format(calcolaArea())		;
-		
+				"\n"	+ "Perimetro: "	+ cm.format(calcolaPerimetro())	+
+				"\n"	+ "Area: "		+ cm2.format(calcolaArea())		;	
 	}
 
 }
